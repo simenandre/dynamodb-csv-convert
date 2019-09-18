443abcfddf6a22257dcd9d21013cca2da3f9f8b6
Simen A. W. Olsen <so@ayr.no>
2019-09-18 13:40:55 +0200

    first release

---
+++ README.md
@@ -0,0 +1,66 @@
+# dynamodb-csv-convert
+
+(WIP) CLI-utility to convert DynamoDB CSV exports done in AWS Console
+
+[![npm version](https://badge.fury.io/js/dynamodb-csv-convert.svg)](https://badge.fury.io/js/dynamodb-csv-convert)
+
+PLEASE DO NOT USE THIS YET. It's *not* done yet.
+
+
+## Usage
+
+This line will convert the file to json.
+
+```bash
+$ npx dynamodb-csv-convert file.csv > file.json
+```
+
+### Parameters
+
+[describe what parameters your package/function takes]
+
+| Parameter | Description                                |
+| :-------- | :----------------------------------------- |
+| `foo`     | A foo thingie (boolean). Default = `true`. |
+| `bar`     | A bar thingie (string). Default = `bar`.   |
+
+### Return
+
+[describe what is returned]
+
+| Key    | Description                                     |
+| :----- | :---------------------------------------------- |
+| `blah` | A boolean containing the current value of blah. |
+
+## Example
+
+[Write something here describing the sample code below. The example code SHOULD WORK!]
+
+```jsx
+import React from 'react';
+import { createThingie } from 'dynamodb-csv-convert';
+
+const Thingie = createThingie();
+
+const MyThingie = () => {
+  // Do some stuff
+
+  return <Thingie>Return some JSX</Thingie>;
+};
+
+export default MyThingie;
+```
+
+## Live demo
+
+You can view/edit the dynamodb-csv-convert demo app on CodeSandbox.
+
+[![Edit demo app on CodeSandbox](https://codesandbox.io/static/img/play-codesandbox.svg)](https://codesandbox.io/s/github/donavon/use-step-multi-step-form-demo/tree/master/?module=%2Fsrc%2FDarkModeToggle.jsx)
+
+## License
+
+**[MIT](LICENSE)** Licensed
+
+---
+
+[Optional footer information here. Maybe thank a friend. Maybe plug your Twitter account. Whatever.]
--- convert.js
+++ convert.js
@@ -1,35 +1,29 @@
-const fs = require('fs');
-const projects = require('./projects.json');
-const moment = require('moment');
-const users = require('./users.json');
-const newUsers = require('./new-users.json');
+const csv = require('csvtojson');
 
-const newProjects = projects.map(project => {
-    const newProject = {};
-    Object.keys(project).map(key => {
-        const newKey = key.replace(/ \(S\)/, '').replace(/ \(BOOL\)/, '').replace(/ \(N\)/, '');
-        if (project[key] && key.match(/ \(S\)/)) {
-          newProject[newKey] = project[key];
-        }
-        if (project[key] && key.match(/ \(BOOL\)/)) {
-          newProject[newKey] = project[key] == "true" ? true : false;
-        }
-        if (project[key] && key.match(/ \(N\)/)) {
-          newProject[newKey] = parseInt(project[key]);
-        }
+(async () => {
+  // find first argument (the file)
+  const [bin, sourcePath, filePath] = process.argv;
+  if (!filePath) {
+    return console.log('Missing file attribute. Usage: npx dynamodb-db-convert file.csv');
+  }
+  if (!filePath.match(/.csv/)) {
+    return console.log('Looks like you are trying to convert something else than csv?')
+  }
+  const jsonFile = await csv().fromFile(filePath);
+  console.log(JSON.stringify(jsonFile.map(line => {
+    const newLine = {};
+    Object.keys(line).map(key => {
+      const attr = key.replace(/ \(S\)/, '').replace(/ \(BOOL\)/, '').replace(/ \(N\)/, '');
+      if (line[key] && key.match(/ \(S\)/)) {
+        newLine[attr] = line[key];
+      }
+      if (line[key] && key.match(/ \(BOOL\)/)) {
+        newLine[attr] = line[key] == "true" ? true : false;
+      }
+      if (line[key] && key.match(/ \(N\)/)) {
+        newLine[attr] = parseInt(line[key]);
+      }
     });
-
-    newProject.timestamp = moment(newProject.createdAt).unix();
-
-    // Find matching user
-    const oldUser = users.find(user => user["id (S)"] === newProject.userId);
-    if(!oldUser) return;
-    const newUser = newUsers.find(user => user['name (S)'] === oldUser['name (S)']);
-    if(!newUser) return console.error('New User not found.', oldUser);
-
-    newProject.userId = newUser['id (S)'];
-
-    return newProject;
-}).filter(project => project);
-
-fs.writeFileSync('new-prod-projects.json', JSON.stringify(newProjects, null, 2));
+    return newLine;
+  }).filter(project => project), null, 2));
+})();
