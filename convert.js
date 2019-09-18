94e7e13dce73b99f5fc2abdd40a51e8427dff681
Simen A. W. Olsen <so@ayr.no>
2019-09-18 13:59:29 +0200

    feat: Add M

--- convert.js
+++ convert.js
@@ -1,29 +1,40 @@
 const csv = require('csvtojson');
+const AWS = require('aws-sdk');
 
 module.exports = async () => {
-  // find first argument (the file)
+
   const [bin, sourcePath, filePath] = process.argv;
+
   if (!filePath) {
     return console.log('Missing file attribute. Usage: npx dynamodb-db-convert file.csv');
   }
   if (!filePath.match(/.csv/)) {
     return console.log('Looks like you are trying to convert something else than csv?')
   }
-  const jsonFile = await csv().fromFile(filePath);
-  console.log(JSON.stringify(jsonFile.map(line => {
-    const newLine = {};
-    Object.keys(line).map(key => {
-      const attr = key.replace(/ \(S\)/, '').replace(/ \(BOOL\)/, '').replace(/ \(N\)/, '');
-      if (line[key] && key.match(/ \(S\)/)) {
-        newLine[attr] = line[key];
-      }
-      if (line[key] && key.match(/ \(BOOL\)/)) {
-        newLine[attr] = line[key] == "true" ? true : false;
-      }
-      if (line[key] && key.match(/ \(N\)/)) {
-        newLine[attr] = parseInt(line[key]);
+
+  const convertRow = (row) => {
+    const newRow = {};
+    Object.keys(row).map(key => {
+      const attr = key.replace(/ \((.*)\)/, '');
+      if (row[key]) {
+        if (key.match(/ \(S\)/)) {
+          newRow[attr] = row[key];
+        } else if (key.match(/ \(BOOL\)/)) {
+          newRow[attr] = row[key] == "true" ? true : false;
+        } else if (key.match(/ \(N\)/)) {
+          newRow[attr] = parseInt(row[key]);
+        } else if (key.match(/ \(M\)/)) {
+          newRow[attr] = AWS.DynamoDB.Converter.unmarshall(JSON.parse(row[key]));
+        } else {
+          newRow[attr] = row[key];
+        }
       }
     });
-    return newLine;
-  }).filter(project => project), null, 2));
+    return newRow;
+  }
+
+  const jsonFile = await csv().fromFile(filePath);
+  console.log(
+    JSON.stringify(jsonFile.map(convertRow).filter(project => project), null, 2)
+  );
 };
